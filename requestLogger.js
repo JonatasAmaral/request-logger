a6ab6813f606ee2d7738fedb9145781be81435e9
Jonatas Amaral <jonataspadtab@gmail.com>
2022-01-15 22:52:18 -0300

    create logs counter functionality

--- requestLogger.js
+++ requestLogger.js
@@ -18,6 +18,24 @@ function consoleLogger(req) {
 	console.log(data);
 }
 
+const logsCounter = {
+	_count: 0,
+	get count() {
+		return this._count;
+	},
+	set count(value) {
+		this._count = value;
+		fs.writeFile(`_logs-counter.txt`, `${this._count}`, err => {
+			if (err) console.error(`Couldn't persist "_logs-counter.txt" file!`);
+		});
+	},
+}
+try {
+	logsCounter.count = parseInt(fs.readFileSync("_logs-counter.txt", "utf8"));
+} catch (err) {
+	console.error("Couldn't read \"_logs-counter.txt\" file!");
+}
+
 function fileLogger(req, res) {
 	let data = dataFetch(req);
 	let dateTime = new Date()
@@ -28,11 +46,14 @@ function fileLogger(req, res) {
 		if (err) throw err;
 	});
 	fs.writeFile(
-		`logs/request-${dateTime}.json`,
+		`logs/request#${logsCounter.count + 1}-${dateTime}.json`,
 		data,
 		err => {
 			if (err) res.status(500).send("Fail to write log file!");
-			else res.send("Log file saved!");
+			else {
+				++logsCounter.count
+				res.send("Log file saved!");
+			}
 		}
 	);
 }
